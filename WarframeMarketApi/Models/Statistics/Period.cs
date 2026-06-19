39aba056f7fea07585b286d151e936ea6b97b49f
1622989459@qq.com <1622989459@qq.com>
2026-06-19 09:17:37 +0800

    chore: Period 还原为和原版一样的 JsonPropertyName 属性，删掉无用的转换器

--- WarframeMarketApi/Models/Statistics/Period.cs
+++ WarframeMarketApi/Models/Statistics/Period.cs
@@ -8,40 +8,7 @@ namespace zms9110750.WarframeMarketApi.Models.Statistics;
 /// </summary>
 /// <param name="Hour48">48 小时内数据，每 2h 跨度</param>
 /// <param name="Day90">90 天内数据，每天跨度</param>
-[JsonConverter(typeof(PeriodConverter))]
 public record Period(
-	Entry[] Hour48,
-	Entry[] Day90
+	[property: JsonPropertyName("48hours")] Entry[] Hour48,
+	[property: JsonPropertyName("90days")] Entry[] Day90
 );
-
-internal class PeriodConverter : JsonConverter<Period>
-{
-	public override Period? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-	{
-		if (reader.TokenType != JsonTokenType.StartObject) return null;
-		Entry[]? h48 = null, d90 = null;
-		while (reader.Read())
-		{
-			if (reader.TokenType == JsonTokenType.EndObject) break;
-			if (reader.TokenType == JsonTokenType.PropertyName)
-			{
-				var name = reader.GetString();
-				reader.Read();
-				if (name == "48hours") h48 = JsonSerializer.Deserialize<Entry[]>(ref reader, options);
-				else if (name == "90days") d90 = JsonSerializer.Deserialize<Entry[]>(ref reader, options);
-				else reader.Skip();
-			}
-		}
-		return new Period(h48 ?? Array.Empty<Entry>(), d90 ?? Array.Empty<Entry>());
-	}
-
-	public override void Write(Utf8JsonWriter writer, Period value, JsonSerializerOptions options)
-	{
-		writer.WriteStartObject();
-		writer.WritePropertyName("48hours");
-		JsonSerializer.Serialize(writer, value.Hour48, options);
-		writer.WritePropertyName("90days");
-		JsonSerializer.Serialize(writer, value.Day90, options);
-		writer.WriteEndObject();
-	}
-}
